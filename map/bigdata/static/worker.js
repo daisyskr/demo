c90e98a047e73efd62cbb7ab879d9290d61d694a
daisyskr <3218387007@qq.com>
2024-03-25 18:30:36 +0800

    bigdata

---
+++ map/bigdata/static/worker.js
@@ -0,0 +1,13 @@
+// 通过importScripts引入.js文件
+importScripts('libs/turf.min.js')
+;(() => {
+  console.time('worker计算点位')
+  // 监听 main 并将缓冲区转移到 worker
+  self.onmessage = function handleMessageFromMain(msg) {
+    console.log('message from main received in worker:', msg.data)
+    let [number, bbox] = msg.data
+    let points = turf.randomPoint(number, { bbox })
+    self.postMessage(points)
+  }
+  console.timeEnd('worker计算点位')
+})()
