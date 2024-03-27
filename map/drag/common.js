e585831c435a422c417fad9e69f28f00419bd11d
daisyskr <3218387007@qq.com>
2024-03-27 18:55:53 +0800

    拖拽

---
+++ map/drag/common.js
@@ -0,0 +1,42 @@
+// export const describe: string = '通用前端方法'
+/**
+ * @description 生成一个唯一的uuid方法
+ * @param {number} len - uuid长度
+ * @param {number} radix - 可选，从62个数字大小写字母中截取radix个作为选择池
+ * @return {*}
+ * @example
+ * ```ts
+ *   gs3d.util.common.getUuid(8)
+ * ```
+ */
+const getUuid = (len, radix) => {
+  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
+  var uuid = []
+  var i
+  radix = radix || chars.length
+  if (len) {
+    // Compact form
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)]
+  } else {
+    // rfc4122, version 4 form
+    var r
+
+    // rfc4122 requires these characters
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-'
+    uuid[14] = '4'
+
+    // Fill in random data.  At i==19 set the high bits of clock sequence as
+    // per rfc4122, sec. 4.1.5
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | (Math.random() * 16)
+        uuid[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r]
+      }
+    }
+  }
+  return uuid.join('')
+}
+
+export default common = {
+  getUuid
+}
---
+++ map/drag/drag.js
@@ -0,0 +1,143 @@
+import Entitys from './entitys.js'
+//鼠标拖动实体
+ export default class dragEntity {
+  /*callback返回三种类型，对应被拖拽实体的三种状态，
+  'move-drag'：entity被拖拽时实时回调，即拖着entity移动时
+  'per-stop-drag'：每次entity拖拽暂时结束时的回调，即左键松开
+  'stop-drag'：所有entity拖拽全部结束时的回调，即拖拽完成，拖拽事件销毁
+  */
+  constructor(val) {
+    val.viewer = val?.viewer ?? variable.viewer
+    this.callback = val.callback ?? (() => {})
+    this.viewer = val.viewer
+    this.movedCartesian = []
+    this.movedPosition = []
+    this.currentId = ''
+    this.currentEntityId = ''
+    this.leftDownFlag = false
+    this.pick = null //储存实体
+    this.handler = new Cesium.ScreenSpaceEventHandler(this.viewer.scene.canvas)
+    this.entitys = new Entitys(this.viewer)
+    if (!this.resultTip) this.resultTip = this.entitys.createMsgTip()
+    document.body.style.cursor = 'pointer'
+    this.handlers()
+  }
+  handlers() {
+    this.leftDownAction()
+    this.mouseMoveAction()
+    this.leftUpAction()
+    this.rightClickAction()
+  }
+  leftDownAction() {
+    let _this = this
+    _this.handler.setInputAction(function (movement) {
+      let pick = _this.viewer.scene.pick(movement.position)
+      if (Cesium.defined(pick) && pick.id.id) {
+        _this.currentId = pick.id.id
+        _this.currentEntityId = pick?.id?.entityId || pick?.id?._entityId
+        _this.pick = pick
+        _this.leftDownFlag = true
+        _this.viewer.scene.screenSpaceCameraController.enableRotate = false //锁定相机
+      }
+    }, Cesium.ScreenSpaceEventType.LEFT_DOWN)
+  }
+  mouseMoveAction(tip = '左键拖动，右键停止') {
+    let _this = this
+    _this.handler.setInputAction(function (movement) {
+      let cartesian = null
+      let ray = _this.viewer.camera.getPickRay(movement.endPosition)
+      cartesian = _this.viewer.scene.globe.pick(ray, _this.viewer.scene)
+      _this.entitys.showTip(_this.resultTip, true, cartesian, tip)
+      if (_this.leftDownFlag === true && _this.pick != null) {
+        _this?.pick?.id && (_this.pick.id.position = cartesian) //此处根据具体entity来处理，也可能是pointDraged.id.position=cartesian;
+        // _this.movedCartesian[_this.currentId] = cartesian
+        let currentItem = _this.movedCartesian.find((item) => {
+          return item.id == _this.currentId
+        })
+        if (currentItem) {
+          currentItem.cartesian = cartesian
+        } else {
+          _this.movedCartesian.push({
+            id: _this.currentId,
+            entityId: _this.currentEntityId,
+            cartesian: cartesian
+          })
+        }
+        _this.callback('move-drag')
+      }
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE)
+  }
+  leftUpAction() {
+    let _this = this
+    _this.handler.setInputAction(function (movement) {
+      if (_this.leftDownFlag === true && _this.pick != null) {
+        _this.leftDownFlag = false
+        _this.pointDraged = null
+        _this.viewer.scene.screenSpaceCameraController.enableRotate = true //解锁相机
+        _this.callback('per-stop-drag')
+      }
+    }, Cesium.ScreenSpaceEventType.LEFT_UP)
+  }
+  rightClickAction() {
+    let _this = this
+    _this.handler.setInputAction(function (movement) {
+      _this.destroyAction()
+      _this.callback('stop-drag')
+    }, Cesium.ScreenSpaceEventType.RIGHT_CLICK)
+  }
+  //清除鼠标事件
+  destroyAction() {
+    this.handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOWN)
+    this.handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_UP)
+    this.handler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE)
+    this.handler.removeInputAction(Cesium.ScreenSpaceEventType.RIGHT_CLICK)
+    this.entitys.remove(this.resultTip)
+    this.resultTip = null
+    document.body.style.cursor = 'default'
+  }
+  getMovedPosition() {
+    this.movedPosition = []
+    let ellipsoid = this.viewer.scene.globe.ellipsoid
+    // for (let key in this.movedCartesian) {
+    //   let cartographic = ellipsoid.cartesianToCartographic(this.movedCartesian[key])
+    //   let lat = Cesium.Math.toDegrees(cartographic.latitude)
+    //   let lon = Cesium.Math.toDegrees(cartographic.longitude)
+    //   let alt = cartographic.height
+    //   this.movedPosition[key] = [lon, lat]
+    // }
+
+    this.movedCartesian.forEach((item) => {
+      let cartographic = ellipsoid.cartesianToCartographic(item.cartesian)
+      let lat = Cesium.Math.toDegrees(cartographic.latitude)
+      let lon = Cesium.Math.toDegrees(cartographic.longitude)
+      let alt = cartographic.height
+      this.movedPosition.push({
+        id: item.id,
+        entityId: item.entityId,
+        coordinate: [lon, lat]
+      })
+    })
+    return this.movedPosition
+  }
+  // 鼠标拾取三维坐标点
+  getCatesian3FromPX(px) {
+    var picks = this.viewer.scene.drillPick(px)
+    this.viewer.render()
+    var cartesian
+    var isOn3dtiles = false
+    for (var i = 0; i < picks.length; i++) {
+      if (picks[i].primitive instanceof Cesium.Cesium3DTileset) {
+        //模型上拾取
+        isOn3dtiles = true
+      }
+    }
+    if (isOn3dtiles) {
+      cartesian = this.viewer.scene.pickPosition(px)
+    } else {
+      var ray = this.viewer.camera.getPickRay(px)
+      if (!ray) return null
+      cartesian = this.viewer.scene.globe.pick(ray, this.viewer.scene)
+    }
+    return cartesian
+  }
+}
